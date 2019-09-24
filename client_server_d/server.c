0b512931450e00e218673f1ed1619fd48c8dc57c
Spence Carrillo <sdc2@williams.edu>
2019-09-24 16:20:10 +0100

    started on client_server

---
+++ client_server_d/server.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <ctype.h>
+
+int main(int argc, char **argv){
+  
+}
