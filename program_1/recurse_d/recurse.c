7ecced42237d2c132bcbf65ecdb305b381c38fad
Spence Carrillo <sdc2@williams.edu>
2019-09-24 13:28:45 +0100

    initial commit

---
+++ program_1/recurse_d/recurse.c
@@ -0,0 +1,37 @@
+
+int main(int argc, char **argv){
+  if(argc < 2 || argc > 2){
+    perror("Please include only one command-line argument, a positive integer n\n");
+    exit(1);
+  }else{
+    int pipe_one[2];
+    if(pipe(pipe_one) == -1){
+      perror("failed to open pipe\n");
+      exit(1);
+    }
+    pid_t pid = fork();
+    if(pid == -1){
+      perror("failed to fork\n");
+      exit(1);
+    }
+
+    char* input = argv[1];
+
+    for (int i = 0; i < strlen(input); i++){
+      if (input[i] > '9' || input[i] < '0'){
+	perror("invalid input. must input positive integer n\n");
+	exit(1);
+      }
+    }
+
+    if(pid == 0){
+      /* write n to pipe */
+      /* call recursive function with pipe */
+      /* recursive function will fork and call itself with n-1 */
+    }else{
+      /* wait for pipe result? */
+      wait(NULL);
+    }
+
+  }
+}
