a3ebccbbe28d60df9d7a2d9d31018c16c9ce4726
Spence Carrillo <sdc2@williams.edu>
2019-09-25 22:36:26 +0100

    passing PID from client to server

--- client_server_d/server.c
+++ client_server_d/server.c
@@ -153,8 +153,7 @@ int main(int argc, char **argv){
     printf("child_ID_str: %s\n", child_ID_str);
 
     //fifo name: server_to_client_{PROCESS_ID}
-    char server_to_client_ID[PID_STRLEN + PATH_SIZE];
-    strcat(server_to_client_ID, FILE_PRE);
+    char server_to_client_ID[PID_STRLEN + PATH_SIZE] = FILE_PRE;
     strcat(server_to_client_ID, child_ID_str);
 
     printf("server_t_c ID: %s\n", server_to_client_ID);
