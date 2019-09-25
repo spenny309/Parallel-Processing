72398134f5c8546ba24e302bf86f1106bc8263d2
Spence Carrillo <sdc2@williams.edu>
2019-09-25 23:52:48 +0100

    client-server operational. needs documentation

--- pargrep_d/pargrep.c
+++ pargrep_d/pargrep.c
@@ -8,61 +8,66 @@
 
 void search_in_file(FILE *fp, char* search_term, char* file_name){
   int current_line = 0;
-  //printf("entering while\n");
+  int buff_size = 2;
+  char* output = (char*)malloc(sizeof(char) * buff_size);
+  output[0] = '\0';
+//search file line by line, looking for strstr match
   while(!feof(fp)){
-    //printf("in while\n");
     char* buffer = NULL;
     size_t n = 0;
     ssize_t line_length = getline(&buffer, &n, fp);
-    //printf("GOT LINE\n");
     if (ferror(fp)){
-      //printf("ERROR\n");
       perror("error reading");
       exit(1);
     }
-    //printf("LINE: %s\n", buffer);
-    //fwrite(buffer, 1, line_length, stdout);
-    //printf("\n");
     if (strstr(buffer, search_term) != NULL){
-      //printf("found a match\n");
+      while(strlen(output) + line_length > buff_size){
+        buff_size *= 2;
+        realloc(output, buff_size);
+      }
+
+      strcat(output, file_name);
+      strcat(output, ": ");
+      strcat(output, buffer);
+      strcat(output, "\n");
+      /*
       printf("%s: ", file_name);
       fwrite(buffer, 1, line_length, stdout);
       if(line_length > 0 && buffer[line_length - 1] != '\n') {
-	printf("\n");
-      }
+        printf("\n");
+      }*/
     }
     free(buffer);
   }
+  printf("%s", output);
+  free(output);
 }
 
 int main(int argc, char **argv){
   switch(argc) {
-  case 0:
-  case 1:
+    case 0:
+    case 1:
     printf("to use: ./pargrep term file_1 file_2 ...\n");
     exit(1);
-  default:
-    //printf("entering for loop\n");
+    default:
+    //fork once per file specified in argument
     for (int i = 2; i < argc; i++){
-      printf("forking\n");
       pid_t pid = fork();
-      //printf("i am process: %d\n", pid);
       if (pid == 0){
-	FILE *fp = fopen(argv[i], "r");
-	if (fp == NULL) {
-	  perror("cannot find/open file");
-	  exit(1);
-	}
-	//printf("searching in file: %s for %s\n", argv[i], argv[1]);
-	search_in_file(fp, argv[1], argv[i]);
-	fclose(fp);
-	printf("finishing fork\n");
-	exit(0);
+        //each child process will search its respective file
+        FILE *fp = fopen(argv[i], "r");
+        if (fp == NULL) {
+          perror("cannot find/open file");
+          exit(1);
+        }
+        search_in_file(fp, argv[1], argv[i]);
+        fclose(fp);
+        exit(0);
       }
     }
   }
+  //wait for each child
   for (int i = 2; i < argc; i++) {
-    //printf("waiting\n");
     wait(NULL);
   }
   exit(0);
