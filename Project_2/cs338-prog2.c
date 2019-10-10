e9b38cb62710be9a6d569a19ccfa1aafc7b84137
Spence Carrillo <sdc2@williams.edu>
2019-10-10 19:16:47 -0400

    bug fixes

--- Project_2/cs338-prog2.c
+++ Project_2/cs338-prog2.c
@@ -267,9 +267,7 @@ printf("perimeter 2\n");
 			}
 			for(k=0; k < from->num_components; k++){
 				//normalize values
-				if(perimeter_pixel_weight > 0){
-					RGB_values[k] /= perimeter_pixel_weight;
-				}
+				RGB_values[k] /= perimeter_pixel_weight;
 				to->row_pointers[i][(from->num_components) * j + k] = RGB_values[k];
 			}
 		}
