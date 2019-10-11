80fb841c37fc933f43edfd3a2e8075465e127738
Spence Carrillo <sdc2@williams.edu>
2019-10-10 21:08:44 -0400

    parameter change

--- Project_2/cs338-prog2.c
+++ Project_2/cs338-prog2.c
@@ -52,8 +52,8 @@
  */
 #define OUT_QUALITY		75
 
-//#define ROW_OR_COL //1 = ROW, 0 = COL
-#define SEQ_OR_INT 1 //1 = sequential, 0 = interleaved
+//#define ROW_OR_COL //defined = ROW, undef = COL
+//#define SEQ_OR_INT 1 //defined = chunk, undef = interleaved
 #define USE_NUM_PROCS 1
 
 /************************* END OF PARAMETERS *******************/
