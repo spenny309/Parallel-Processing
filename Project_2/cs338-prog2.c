c2cae08405cd4e73763df64237da5da607a6dc39
Spence Carrillo <sdc2@williams.edu>
2019-10-15 20:16:51 +0100

    updating folder

--- Project_2/cs338-prog2.c
+++ Project_2/cs338-prog2.c
@@ -197,6 +197,8 @@ void *CS338_row_seq(void *proc_num){
 		}
 	}
 
+	printf("finished top and bottom border for processor: %d", thread_num);
+
 	//left and right perimeter of image
 	for(i = radius + (thread_num * row_chunk_size); i < radius + ((thread_num+1) * row_chunk_size); i++){
 		for(j=0; j < from->image_width; j++){
