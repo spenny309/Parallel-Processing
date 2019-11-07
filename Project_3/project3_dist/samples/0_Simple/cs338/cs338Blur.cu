d94d75fb1e5b2afedaac1df09fc39da858cf0565
Spence Carrillo <sdc2@williams.edu>
2019-11-07 01:46:19 +0000

    fixes

--- Project_3/project3_dist/samples/0_Simple/cs338/cs338Blur.cu
+++ Project_3/project3_dist/samples/0_Simple/cs338/cs338Blur.cu
@@ -323,8 +323,8 @@ __global__ void cs338Blur(unsigned char* from, unsigned char* to, int r,
 {
   //printf("blockIdx.x : %d \t blockDim.x : %d \t threadIdx.x : %d \n blockIdx.y : %d \t blockDim.y : %d \t threadIdx.y : %d \n\n", blockIdx.x, blockDim.x, threadIdx.x, blockIdx.y, blockDim.y, threadIdx.y);
 
-  int row = (blockIdx.x * blockDim.x + threadIdx.x);
-  int col = (blockIdx.y * blockDim.y + threadIdx.y);
+  int col = (blockIdx.x * blockDim.x + threadIdx.x);
+  int row = (blockIdx.y * blockDim.y + threadIdx.y);
   int this_pixel = (col * row) + row;
 
 //If current pixel is invalid, do nothing
