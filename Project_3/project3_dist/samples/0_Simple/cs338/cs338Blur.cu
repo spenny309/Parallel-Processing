91d3768edabab9f61a89a8ca1cc748d969f9c281
Spence Carrillo <sdc2@williams.edu>
2019-11-13 01:54:18 +0000

    bug fixes

--- Project_3/project3_dist/samples/0_Simple/cs338/cs338Blur.cu
+++ Project_3/project3_dist/samples/0_Simple/cs338/cs338Blur.cu
@@ -646,7 +646,6 @@ runKernel(frame_ptr result)
          formats are rarely more rectangular than 4:3 or 16:9
          */
          //Add define value for block dimensions
-  int square_dimension = ceil(sqrt(max_of_width_and_height));
   double block_size = 32.0;
   dim3 dim_grid(ceil(max_of_width_and_height / block_size), ceil(max_of_width_and_height / block_size), 1);
   dim3 dim_block(block_size, block_size, 1);
